ccbf35b991f426968ea82580c03f1472f46fc33d
chaya2z <taisuke.nakano@chaya2z.com>
2024-04-28 14:54:49 +0900

    deno fmt

--- main.ts
+++ main.ts
@@ -1,34 +1,34 @@
-import { Octokit } from "https://esm.sh/octokit?dts"
+import { Octokit } from "https://esm.sh/octokit?dts";
 
 const main = async () => {
-  const owner = "chaya2z"
-  const repo = "ts-commit-action"
+  const owner = "chaya2z";
+  const repo = "ts-commit-action";
 
   // authentication
   const octokit = new Octokit({
-    auth: Deno.env.get("GITHUB_TOKEN")
+    auth: Deno.env.get("GITHUB_TOKEN"),
   });
 
   // for debugging
-  const {
-    data: {login},
-  } = await octokit.rest.users.getAuthenticated();
+  const { data: { login } } = await octokit.rest.users.getAuthenticated();
   console.log("Hello, %s", login);
 
-  const { data: {object: {sha: base_commit_sha}}} = await octokit.rest.git.getRef({
-    owner,
-    repo,
-    ref: "heads/main",
-  });
+  const { data: { object: { sha: base_commit_sha } } } = await octokit.rest.git
+    .getRef({
+      owner,
+      repo,
+      ref: "heads/main",
+    });
 
-  const { data: {tree: {sha: base_tree_sha}} } = await octokit.rest.git.getCommit({
-    owner,
-    repo,
-    commit_sha: base_commit_sha,
-  });
+  const { data: { tree: { sha: base_tree_sha } } } = await octokit.rest.git
+    .getCommit({
+      owner,
+      repo,
+      commit_sha: base_commit_sha,
+    });
 
   // create tree
-  const { data: {sha: tree_sha}} = await octokit.rest.git.createTree({
+  const { data: { sha: tree_sha } } = await octokit.rest.git.createTree({
     owner,
     repo,
     base_tree: base_tree_sha,
@@ -36,8 +36,8 @@ const main = async () => {
       {
         path: "test.txt",
         mode: "100644",
-        content: "test content 04/28 12:00"
-      }
+        content: "test content 04/28 12:00",
+      },
     ],
   });
   console.log("Tree was created", tree_sha);
@@ -49,23 +49,23 @@ const main = async () => {
     message: "Hello World!",
     tree: tree_sha,
     parents: [
-      base_commit_sha
-    ]
-  })
+      base_commit_sha,
+    ],
+  });
   if (status === 201) {
     console.log("Commit: %s", sha);
   }
 
   // push
-  const { data} = await octokit.rest.git.updateRef({
+  const { data } = await octokit.rest.git.updateRef({
     owner,
     repo,
     ref: "heads/main",
     sha,
   });
   console.log(data);
-  console.log("success")
-}
+  console.log("success");
+};
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
